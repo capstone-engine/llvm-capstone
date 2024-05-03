4d5ca55385c196763977d883a4a33d2da957f6a2
Jiajie Chen <c@jia.je>
2024-05-03 19:17:19 +0800

    Handle INVALID_SIMPLE_VALUE_TYPE in getEnumName

--- llvm/utils/TableGen/CodeGenTarget.cpp
+++ llvm/utils/TableGen/CodeGenTarget.cpp
@@ -264,6 +264,7 @@ StringRef llvm::getEnumName(MVT::SimpleValueType T) {
   case MVT::Untyped:   return "MVT::Untyped";
   case MVT::funcref:   return "MVT::funcref";
   case MVT::externref: return "MVT::externref";
+  case MVT::INVALID_SIMPLE_VALUE_TYPE: return "MVT::INVALID_SIMPLE_VALUE_TYPE";
   default: llvm_unreachable("ILLEGAL VALUE TYPE!");
   }
   // clang-format on
