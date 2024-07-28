3219b645fa4f3cdc9aa5e52dee390528396168bd
wargio <wargio@libero.it>
2024-07-28 14:12:25 +0800

    [mips] Add OPERAND_MEM_SIMM9

--- llvm/utils/TableGen/PrinterCapstone.cpp
+++ llvm/utils/TableGen/PrinterCapstone.cpp
@@ -2764,6 +2764,9 @@ std::string getPrimaryCSOperandType(Record const *OpRec) {
     return "CS_OP_INVALID";
   else if (OperandType == "OPERAND_IMPLICIT_IMM_0")
     return "CS_OP_IMM";
+  // Mips
+  else if (OperandType == "OPERAND_MEM_SIMM9")
+    return "CS_OP_IMM";
   else
     PrintFatalNote("Unhandled OperandType: " + OperandType);
   return OperandType;
